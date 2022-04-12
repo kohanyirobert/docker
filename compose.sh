7f98c25447184cab3af42f34655a40172a74364e
Kohányi Róbert <kohanyi.robert@gmail.com>
2022-04-12 22:30:09 +0200

    Keep original docker-compose file in .cache
    Docker found the binary ending in x86_64 and barked about that it's not a valid plugin name.

--- compose.sh
+++ compose.sh
@@ -2,13 +2,15 @@ compose_version=2.4.1
 compose_os=linux
 compose_arch=$(arch)
 compose_binary=docker-compose-$compose_os-$compose_arch
+compose_url=https://github.com/docker/compose/releases/download/v$compose_version/$compose_binary
 plugins_dir=$HOME/.docker/cli-plugins
 plugin_path=$plugins_dir/docker-compose
 mkdir -p $plugins_dir
-cd $plugins_dir
+cd $HOME/.cache
 wget \
+  --no-verbose \
   --timestamping \
-  https://github.com/docker/compose/releases/download/v$compose_version/$compose_binary
+  $compose_url
 chmod +x $compose_binary
-ln -sf $compose_binary docker-compose
+ln -srf $compose_binary $plugin_path
 cd ~
